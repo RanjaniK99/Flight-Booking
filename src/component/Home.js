660a03020546da7a6c23e5ad66604e504ec1a63a
Ranjani <ranjanikarthi01@gmail.com>
2022-02-26 21:31:48 -0800

    Header page added

---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <section>
+        <div class="bg1">
+          <div class="road"></div>
+          <img src="../assets/airplane.png" alt="" class="flight" />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
